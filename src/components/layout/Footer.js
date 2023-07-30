a1c199d4bdbd86c01de266c99783ee602974c3f4
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2023-07-30 17:06:36 -0300

    Home e LinkButton

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,9 +5,15 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <ul className={styles.social_list}>
-        <li> <FaFacebook /> </li>
-        <li> <FaInstagram /> </li>
-        <li> <FaLinkedin />  </li>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
       </ul>
       <p className={styles.copy_right}>
         <span>SmartSpend</span> &copy; 2023
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,20 @@
-function Home(){
-    return <h1>Home</h1>
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
+
+
+function Home() {
+  return (
+    <section className={styles.home_container}>
+      <h1>
+        Bem-vindo ao <span>SmartSpend</span>
+      </h1>
+      <p>Inicie o controle dos seus projetos hoje mesmo!</p>
+      <LinkButton to="/newproject" text="Criar projeto" />
+      <img src={savings} alt="Savings" />
+    </section>
+  )
 }
 
 export default Home
