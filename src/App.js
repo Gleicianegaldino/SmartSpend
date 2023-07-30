3ae0a97ef40b28d245ef10f362f43f3ce5eed7af
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2023-07-30 15:11:58 -0300

    Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
+import { BrowserRouter as Router,  Switch , Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Container from './components/layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <p>SmartSpend</p>
+   <Router>
+    <div>
+      <Link to="/">Início</Link>
+      <Link to="/contact">Contato</Link>
+      <Link to="/company">Empresa</Link>
+      <Link to="/newproject">Novo Projeto</Link>
     </div>
-  );
+     <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/"> <Home /> </Route>
+          <Route path="/company"> <Company /> </Route>
+          <Route path="/contact"> <Contact /> </Route>
+          <Route path="/newproject"> <NewProject /> </Route>
+          </Container>
+      </Switch>
+      <p>Footer</p>
+   </Router>
+  )
 }
 
-export default App;
+export default App
